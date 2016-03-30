53f9e0b19417fe49e8f5349dfb50e34feea689f9
18801148360 <704818942@qq.com>
2016-03-30 17:02:38 +0800

    tijiao1
    提交1

---
+++ webmvenDemo/src/main/java/com/cxb/webDemo1.java
@@ -0,0 +1,24 @@
+package com.cxb;
+
+import org.testng.annotations.Test;
+
+public class webDemo1 {
+	
+	@Test
+	public void case1(){
+		
+		
+		//哈个人
+		System.out.println("这是一个case1");
+		      
+	}
+	
+	@Test
+	public void case2(){
+		System.out.println("这是第二个case2");
+	}
+	@Test
+	public void case3(){
+		System.out.println("这是第三个case3");
+	}
+}
